c967715c988ea1ed9479f0e53bccb2bb37325d26
Abbi Nizar <abbinizarm@gmail.com>
2019-02-05 23:29:50 +0700

    Route

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,22 @@
 <template>
-<div>
-<div v-for="thread in threads" class="col-large push-top">
-  <h1> {{thread.title}} </h1>
-  <div class="post-list">
-    <div v-for="postId in thread.posts" class="post">
-      <div class="user-info">
-        <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
-        <a href="#">
-          <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-        </a>
-        <p class="dekstop-only text-small">10</p>
-      </div>
-      <div class="post-content">
-        <div>
-          <p>{{posts[postId].text}}</p>
-        </div>
-      </div>
-      <div class="post-date text-faded">
-        {{posts[postId].publisedhAt}}
-      </div>
-    </div>
-  </div>
+<div class="col-full">
+  <h1> welcome to the forum </h1>
+  <ThreadList :threads="Object.values(threads)"/>
 </div>
-</div>
-  <!-- <div class="hello">
-    <h1>welcome to the forum</h1>
-    <div v-for="thread in threads">
-      <h2>{{thread.title}}</h2>
-      <div v-for="postId in thread.posts">
-        <p>{{users[posts[postId].userId].name}}</p>
-        <p>{{posts[postId].text}}</p>
-      </div>
-    </div>
-  </div> -->
 </template>
 
 <script>
 import sourceData from '@/source'
+import ThreadList from './ThreadList'
 console.log(sourceData) 
 export default {
   name: 'HelloWorld',
+  components: {
+    ThreadList
+  },
   data () {
     return {
-      threads: sourceData.threads,
+      threads: Object.values(sourceData.threads),
       posts:sourceData.posts,
       users:sourceData.users
     }
--- src/components/trash.vue
+++ src/components/trash.vue
@@ -1,10 +1,8 @@
-<template>
-<div  class="col-large push-top">
-    <!-- <h1 v-for="thread in threads"></h1> -->
-    <!-- <div> -->
+menampilkan thread
+<!-- <div v-for="thread in threads" class="col-large push-top">
   <h1> {{thread.title}} </h1>
   <div class="post-list">
-    <div v-for="postId in thread.posts" class="post" >
+    <div v-for="postId in thread.posts" class="post">
       <div class="user-info">
         <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
         <a href="#">
@@ -22,24 +20,16 @@
       </div>
     </div>
   </div>
-</div>
+</div> -->
 
-</template>
-<script>
-import sourceData from '@/source'
-export default {
-    props:{
-        id:{
-            required: true,
-            type: String
-        }
-    },
-    data(){
-        return{
-            thread: sourceData.threads[this.id],
-            posts:sourceData.posts,
-            users:sourceData.users
-        }
-    }
-}
-</script>
+menampilkan hello world
+  <!-- <div class="hello">
+    <h1>welcome to the forum</h1>
+    <div v-for="thread in threads">
+      <h2>{{thread.title}}</h2>
+      <div v-for="postId in thread.posts">
+        <p>{{users[posts[postId].userId].name}}</p>
+        <p>{{posts[postId].text}}</p>
+      </div>
+    </div>
+  </div> -->
